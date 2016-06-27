ccad5b3a8dc3de4d117f99de430be15e3064518b
luca <luca@luca-SATELLITE-L50-A-12W>
2016-06-27 14:39:44 +0200

    classifier

--- src/main/java/it/himyd/spark/ml/classification/StockClassifierSGD.java
+++ src/main/java/it/himyd/spark/ml/classification/StockClassifierSGD.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.mllib.classification.StreamingLogisticRegressionWithSGD;
+import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.streaming.Duration;
@@ -12,6 +13,7 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaPairInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.dstream.DStream;
 
 import it.himyd.analysis.AnalysisRunner;
 import it.himyd.finance.yahoo.Stock;
@@ -20,7 +22,7 @@ import it.himyd.stock.StockVariation;
 
 public class StockClassifierSGD {
 	private final static Duration BATCH_DURATION = Durations.seconds(1);
-	
+
 	private static StreamingLogisticRegressionWithSGD slrModel;
 
 	private static int numFeatures = 2;
@@ -48,15 +50,16 @@ public class StockClassifierSGD {
 		JavaDStream<LabeledPoint> trainingData = getDStreamTraining(variation);
 		trainingData.cache();
 		slrModel.trainOn(trainingData);
-		
+
 		JavaDStream<LabeledPoint> testData = getDStreamTraining(variation);
-		
-		
-//		 slrModel.predictOn(getDStreamPrediction(jssc)));
+
+
+		slrModel.predictOn(getDStreamPrediction(testData)).print();;
 
 		jssc.start();
 		jssc.awaitTermination();
 	}
+
 
 	public static JavaDStream<LabeledPoint> getDStreamTraining(JavaDStream<StockVariation> stocks) {
 
@@ -66,7 +69,7 @@ public class StockClassifierSGD {
 
 			@Override
 			public LabeledPoint call(StockVariation sv) throws Exception {
-				System.out.println("Inside LabeledPoint call : ----- ");
+				//System.out.println("Inside LabeledPoint call : ----- ");
 
 				Double label;
 				Double pv = sv.getPriceVariation();
@@ -78,8 +81,6 @@ public class StockClassifierSGD {
 
 				if (pv > 0) {
 					label = new Double(1);
-				} else if (pv < 0) {
-					label = new Double(2);
 				} else {
 					label = new Double(0);
 				}
@@ -90,26 +91,24 @@ public class StockClassifierSGD {
 		});
 	}
 
-	// public static JavaDStream<Vector>
-	// getDStreamPrediction(JavaStreamingContext context) {
-	// JavaReceiverInputDStream<String> lines =
-	// context.socketTextStream("localhost", 9999);
-	//
-	// return lines.map(new Function<String, Vector>() {
-	//
-	// private static final long serialVersionUID = 1268686043314386060L;
-	//
-	// @Override
-	// public Vector call(String data) throws Exception {
-	// System.out.println("Inside Vector call : ----- ");
-	// String vcS[] = data.split("-");
-	// double vc[] = new double[3];
-	// int i = 0;
-	// for (String vcSi : vcS) {
-	// vc[i++] = Double.parseDouble(vcSi);
-	// }
-	// return Vectors.dense(vc);
-	// }
-	// });
-	// }
+	public static JavaDStream<Vector>
+	getDStreamPrediction(JavaDStream<LabeledPoint> lines) {
+
+		return lines.map(new Function<LabeledPoint, Vector>() {
+
+			private static final long serialVersionUID = 1268686043314386060L;
+
+			@Override
+			public Vector call(LabeledPoint sv) throws Exception {
+//				Double label;
+//				Double pv = sv.features();
+//				Double vv = sv.getVolumeVariation();
+//
+//				double vc[] = new double[numFeatures];
+//				vc[0] = pv;
+//				vc[1] = vv;
+				return sv.features();
+			}
+		});
+	}
 }
